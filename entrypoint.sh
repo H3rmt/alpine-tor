a3c95ce6c5943ba2a55eb47303014264985cbd81
Enrico Stemmer <stemmer.enrico@gmail.com>
2024-05-30 16:18:54 +0000

    added ExtORPort

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,35 +16,38 @@ echo "Searching for environment variable 'mode'"
 # Set required values depending on set mode
 if [ "$mode" = "exit" ]
 then
-   echo "Starting up as exit node (not tested...)"
+  echo "Starting up as exit node (not tested...)"
 
-   echo "ORPort $ORPort" >> /etc/tor/torrc
-   echo "ExitRelay 1" >> /etc/tor/torrc
-   echo "DirPort $DirPort" >> /etc/tor/torrc
+  echo "ORPort $ORPort" >> /etc/tor/torrc
+  echo "ExitRelay 1" >> /etc/tor/torrc
+  echo "SocksPort 0" >> /etc/tor/torrc
+  echo "DirPort $DirPort" >> /etc/tor/torrc
 elif [ "$mode" = "middle" ]
 then
-   echo "Starting up as middle / guard node"
+  echo "Starting up as middle / guard node"
 
-   echo "ORPort $ORPort" >> /etc/tor/torrc
-   echo "ExitRelay 0" >> /etc/tor/torrc
-   echo "SocksPort 0" >> /etc/tor/torrc
-   echo "DirPort $DirPort" >> /etc/tor/torrc
+  echo "ORPort $ORPort" >> /etc/tor/torrc
+  echo "ExitRelay 0" >> /etc/tor/torrc
+  echo "SocksPort 0" >> /etc/tor/torrc
+  echo "DirPort $DirPort" >> /etc/tor/torrc
 elif [ "$mode" = "bridge" ]
 then
   echo "Starting up as bridge node"
 
   echo "ORPort $ORPort" >> /etc/tor/torrc
+  echo "ExitRelay 0" >> /etc/tor/torrc
   echo "BridgeRelay 1" >> /etc/tor/torrc
-  echo "ServerTransportListenAddr obfs4 0.0.0.0:$PTPort" >> /etc/tor/torrc
-  echo "ServerTransportPlugin obfs4 exec /usr/local/bin/obfs4proxy" >> /etc/tor/torrc
+  echo "ExtORPort auto" >> /etc/tor/torrc
+  echo "ServerTransportListenAddr obfs4 $PTPort" >> /etc/tor/torrc
+  echo "ServerTransportPlugin obfs4 exec /usr/local/bin/obfs4proxy -enableLogging" >> /etc/tor/torrc
 elif [ "$mode" = "proxy" ]
 then
   echo "Starting up as proxy"
 
   echo "SOCKSPort 0.0.0.0:$SOCKSPort" >> /etc/tor/torrc
 else
-   echo "No mode set. Please refer to the Readme.md on how to run. Exiting."
-   exit;
+  echo "No mode set. Please refer to the Readme.md on how to run. Exiting."
+  exit;
 fi
 # Additional optional values
 if [ -n "$DNSPort" ]; then
